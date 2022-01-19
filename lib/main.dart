72356acf08b4cd66df84c5f1185b8eae70464789
Michaelemil629 <76790386+Michaelemil629@users.noreply.github.com>
2022-01-19 23:03:09 +0200

    1

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:learn_me_game/screen/Puzzel.dart';
+
+import 'package:learn_me_game/screen/Relatedgame.dart';
+import 'package:learn_me_game/screen/color_related.dart';
+import 'package:learn_me_game/screen/dicegame.dart';
 import 'package:learn_me_game/screen/edit_profile.dart';
 import 'package:learn_me_game/screen/home.dart';
 import 'package:learn_me_game/screen/setting.dart';
 import 'package:learn_me_game/screen/sign_in.dart';
 import 'package:learn_me_game/screen/sign_up.dart';
 import 'package:learn_me_game/screen/splash_screen.dart';
+import 'package:learn_me_game/screen/xo_game.dart';
 import 'package:learn_me_game/widget/constant.dart';
+
+//import 'package:learn_me_game/widget/category_card.dart';
 
 main() {
   runApp(MyApp());
@@ -27,6 +35,11 @@ class MyApp extends StatelessWidget {
         '/signin': (context) => SignIn(),
         '/signup': (context) => SignUp(),
         '/profile': (context) => EditProfile(),
+        '/relatedgame': (context) => Relatedgame(),
+        '/puzzelgame': (context) => PuzzleApp(),
+        '/dicegame': (context) => dicegame(),
+        '/xogame': (context) => xo(),
+        '/colorgame': (context) => colorgame(),
       },
       theme: ThemeData(
         scaffoldBackgroundColor: lightBlue,
---
+++ lib/widget/puzzel_category.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:learn_me_game/widget/constant.dart';
+
+class CategoryCard2 extends StatelessWidget {
+  final String image;
+  final String name;
+  final dynamic color;
+  CategoryCard2(this.image, this.name, this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: this.color,
+      child: InkWell(
+        onTap: () {
+          Navigator.pushNamed(context, '/puzzelgame');
+        },
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/${this.image}'),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 1,
+              child: Container(
+                padding: EdgeInsets.all(10.0),
+                color: black45,
+                width: MediaQuery.of(context).size.width,
+                child: Text(
+                  '${this.name}',
+                  style: TextStyle(
+                    color: white,
+                    fontSize: fontSize20,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
